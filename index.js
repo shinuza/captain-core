c105b12eeffe74b57e0f1450df789d9e92567030
Samori Gorse <samorigorse@gmail.com>
2012-12-15 19:04:05 +0100

    Changed test to mocha, fixed login

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ var middleware = require('./lib/middleware');
 var users = require('./lib/users');
 
 server.use(restify.acceptParser(server.acceptable));
-server.use(restify.authorizationParser());
-server.use(middleware.authorizationHandler(server))
+server.use(restify.bodyParser());
 server.use(restify.dateParser());
 server.use(restify.queryParser());
-server.use(restify.bodyParser());
+server.use(middleware.authorizationHandler(server));
 
-users(server);
+users.attach(server);
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
---
+++ test/users.js
@@ -0,0 +1,40 @@
+var assert = require('assert');
+var restify = require('restify');
+var db = require('riak-js').getClient();
+
+var encode = require('../lib/users').encode;
+
+// Creates a JSON client
+var client = restify.createJsonClient({
+  url: 'http://localhost:8080'
+});
+
+before(function(done) {
+  db.save('users', 'admin', {username: 'admin', password: encode('admin')}, function(err) {
+    if(err) throw err;
+    done();
+  });
+});
+
+describe('Users', function(){
+  it('should not be able to log with wrong credentials', function(done) {
+    client.post('/login', {username: 'pinochio', password: 'foobar'}, function(err, req, res) {
+      assert.equal(res.statusCode, 403);
+      done();
+    });
+  });
+  
+  it('should be able to log with correct credentials', function(done) {
+    client.post('/login', {username: 'admin', password: 'admin'}, function(err, req, res) {
+      assert.equal(res.statusCode, 200);
+      done();
+    });
+  });
+  
+  it('should create users', function(done) {
+    client.post('/users', {username: 'johndoe', password: 'foobar'}, function(err, req, res, obj){
+      if (err) throw err;
+      done();
+    });
+  });
+});
