3862681219e372bd467fd7bb7c137c63ed7b7215
Samori Gorse <samorigorse@gmail.com>
2012-12-20 19:25:45 +0100

    Moved custom middleware to a dedicated module

--- index.js
+++ index.js
@@ -3,33 +3,13 @@ var express = require('express'),
     app = express(),
     settings = require('./settings');
 
+var middleware = require('./lib/middleware');
 var users = require('./lib/users');
 var posts = require('./lib/posts');
 
 app.use(express.bodyParser());
 app.use(express.cookieParser());
-
-app.use(function(req, res, next) {
-  req.user = null;
-  if(!req.cookies.token) {
-    next();
-  } else {
-    users.findByToken(req.cookies.token, function(err, user) {
-      req.user = user;
-      next();
-    });
-  }
-});
-app.use(app.router);
-app.use(function(err, req, res, next) {
-  if(err.statusCode) {
-    res.json(err.statusCode, {error: err.message, statusCode: err.statusCode});
-  } else if(err.syscall) {
-    res.json(500, {error: 'An unexcepted error occured'});
-  } else {
-    next(err);
-  }
-});
+app.use(middleware.authenticate())
 
 app.post('/login', users.login);
 app.post('/logout', users.logout);
