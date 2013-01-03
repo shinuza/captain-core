1ff18562514bcac4231e0331491c2a1ae5492272
Samori Gorse <samorigorse@gmail.com>
2013-01-03 17:26:22 +0100

    Updated settings file

--- settings.js
+++ settings.js
@@ -1,5 +1,7 @@
 exports.SECRET_KEY = '/muuO2DWoWdWcCuMK6jUb+yF6XvE3TajHncvUajqgQv/WYTQG3AbqjjQ';
 exports.HOST = 'localhost';
 exports.PORT = 8080;
-exports.ENGINE = 'redis';
-exports.ENGINE_OPTIONS = {port: 6379};
+exports.DATABASE = {
+  'NAME': __dirname + '/test.sqlite',
+  'ENGINE': 'sqlite'
+};
