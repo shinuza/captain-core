3babdd15c0baefe95bdb14f039964e06a54b535f
Samori Gorse <samorigorse@gmail.com>
2013-01-21 03:33:01 +0100

    Code organization

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-require('express-resource');
-var express = require('express'),
-    app = express(),
-    settings = require('./lib/settings');
-
-var cons = require('consolidate'),
+var resource = require('express-resource'),
+    express = require('express'),
+    cons = require('consolidate'),
     swig = require('swig');
 
-var middleware = require('./lib/middleware');
-var sessions = require('./lib/resources/sessions');
-var users = require('./lib/resources/users');
-var posts = require('./lib/resources/posts');
-var tags = require('./lib/resources/tags');
+var middleware = require('./lib/middleware'),
+    sessions = require('./lib/resources/sessions'),
+    users = require('./lib/resources/users'),
+    posts = require('./lib/resources/posts'),
+    tags = require('./lib/resources/tags');
 
+var app = express(),
+    settings = require('./lib/settings');
+
+var templateDir = settings.get('TEMPLATE_DIR'),
+    mediaRoot = settings.get('MEDIA_ROOT'),
+    siteTitle = settings.get('SITE_TITLE');
+
+// TODO: Remove this
 app.getSettings = function() { return settings; };
 
-var templateDir = settings.get('TEMPLATE_DIR');
+// Templates
 swig.init({
   root: templateDir,
   allowErrors: true // TODO: Only for dev
@@ -23,13 +28,15 @@ app.set('views',templateDir);
 app.set('view engine', 'html');
 app.set('view options', { layout: false });
 app.engine('.html', cons.swig);
-app.locals.siteTitle = settings.get('SITE_TITLE');
+app.locals.siteTitle = siteTitle;
 
-app.use(express.bodyParser({ keepExtensions: true, uploadDir: settings.get('MEDIA_ROOT') }));
+// Middleware
+app.use(express.bodyParser({ keepExtensions: true, uploadDir: mediaRoot }));
 app.use(express.cookieParser());
 app.use(middleware.authenticate());
 app.use(express.logger('tiny'));
 
+// Routes
 app.post('/users/:user/posts', users.posts.associate);
 app.get('/users/:user/posts', users.posts.list);
 app.resource('users', users);
@@ -45,6 +52,7 @@ app.resource('tags', tags);
 
 app.resource('sessions', sessions);
 
+// TODO: Maybe move this up
 app.use(app.router);
 app.use(express.errorHandler()); //TODO: Only for dev, otherwise response with something stupid
 app.use(middleware.notFound());
