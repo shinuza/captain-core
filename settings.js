1b253ba4b9447e9c005e8461705c7cf029ede881
Samori Gorse <samorigorse@gmail.com>
2013-02-01 14:00:40 +0100

    syncdb now uses this any-db

--- settings.js
+++ settings.js
@@ -1,8 +1,7 @@
 exports.SECRET_KEY = '/muuO2DWoWdWcCuMK6jUb+yF6XvE3TajHncvUajqgQv/WYTQG3AbqjjQ';
 exports.HOST = 'localhost';
 exports.PORT = 8080;
-exports.DATABASE = {
-  'NAME': __dirname + '/test.sqlite',
-  'ENGINE': 'sqlite'
-};
+exports.DB = 'mysql://shinuza@localhost/test';
+exports.DB = 'postgres://shinuza@localhost/shinuza';
+exports.DB = 'sqlite3://cool.sqlite';
 exports.DEBUG = true;
