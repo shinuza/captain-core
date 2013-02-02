ce4a110bb04e3ec21e5cd47393d216266339129f
Samori Gorse <samorigorse@gmail.com>
2013-02-02 17:16:34 +0100

    Added tokens tests

--- sql/create/tokens.sql
+++ sql/create/tokens.sql
@@ -2,7 +2,7 @@ CREATE TABLE tokens (
   id SERIAL,
   token VARCHAR(255) NOT NULL UNIQUE,
   created_at TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP,
   user_id INTEGER NOT NULL,
   CONSTRAINT tokens_pkey PRIMARY KEY (id)
 );
---
+++ test/models/3_tokens.js
@@ -0,0 +1,66 @@
+var assert = require('assert');
+var models = require('../../lib/models');
+
+describe('Models', function() {
+
+  describe('Tokens', function() {
+
+    it('create', function(done) {
+      models.tokens.create({token: 'Blablabla', user_id: 1}, function(err, token) {
+        assert.ifError(err);
+        assert.notEqual(token.id, undefined);
+        done();
+      });
+    });
+
+    it('create twice the same token should trigger an error', function(done) {
+      models.tokens.create({token: 'Blablabla', user_id: 1}, function(err) {
+        assert.notEqual(null, err);
+        done();
+      });
+    });
+
+    it('get by id', function(done) {
+      models.tokens.findById(1, function(err, token) {
+        assert.ifError(err);
+        assert.equal(token.token, 'Blablabla');
+        done();
+      });
+    });
+
+    it('update', function(done) {
+      models.tokens.update(1, {'token': 'BOOM'}, function(err, token) {
+        assert.ifError(err);
+        assert.equal(token.token, 'BOOM');
+        done();
+      });
+    });
+
+    it('all', function(done) {
+      models.tokens.all(function(err, tokens) {
+        assert.ifError(err);
+        assert.equal(tokens.length, 1);
+        done();
+      });
+    });
+
+    it('query', function(done) {
+      models.tokens.query('SELECT COUNT(id) FROM tokens', function(err, r) {
+        assert.ifError(err);
+        assert.equal(r.rows[0].count, 1);
+        done();
+      });
+    });
+
+    it('del', function(done) {
+      models.tokens.del(1, function(err, count) {
+        assert.ifError(err);
+        assert.ok(count == 1);
+        done();
+      });
+    });
+
+
+  });
+
+});
