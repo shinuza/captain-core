accf1a9710db62e9e4b7e4a9e48d152a97be24b5
Samori Gorse <samorigorse@gmail.com>
2013-02-02 14:46:51 +0100

    Added post model

--- test/models/1_posts.js
+++ test/models/1_posts.js
@@ -1,9 +1,50 @@
 var assert = require('assert');
 var models = require('../../lib/models');
 
+var bogan = require('boganipsum');
+
 describe('Models', function() {
 
   describe('Posts', function() {
+
+    var posts = {
+      title: 'Foobar',
+      summary: 'Integer posuere erat a ante venenatis dapibus posuere velit aliquet.',
+      body: bogan(),
+      published: true,
+      user_id: 1
+    };
+
+    it('create', function(done) {
+      models.posts.create(posts, function(err, tag) {
+        assert.ifError(err);
+        assert.notEqual(tag.id, undefined);
+        done();
+      });
+    });
+
+    it('create twice the same post should trigger an error', function(done) {
+      models.posts.create(posts, function(err) {
+        assert.notEqual(null, err);
+        done();
+      });
+    });
+
+    it('get by slug', function(done) {
+      models.posts.find('foobar', function(err, post) {
+        assert.ifError(err);
+        assert.equal(post.slug, 'foobar');
+        done();
+      });
+    });
+
+    it('get by id', function(done) {
+      models.posts.find('1', function(err, post) {
+        assert.ifError(err);
+        assert.equal(post.slug, 'foobar');
+        done();
+      });
+    });
 
   });
 
