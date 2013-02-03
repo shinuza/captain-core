48f4875982e9fae9b2daa4e0dc9e5d32d708b345
Samori Gorse <samorigorse@gmail.com>
2013-02-03 14:10:27 +0100

    Added Resource marker

--- test/resources/1_sessions.js
+++ test/resources/1_sessions.js
@@ -2,39 +2,43 @@ var assert = require('assert');
 var client = require('../client');
 var models = require('../../lib/models');
 
-describe('Sessions', function() {
+describe('Resource', function() {
 
-  it('allow to authenticate with wrong credentials', function(done) {
-    client.post('/sessions/', {username: 'pinochio', password: 'foobar'}, function(err, req, res, json) {
-      assert.equal(json.token, undefined);
-      assert.equal(res.statusCode, 403);
-      done();
-    });
-  });
+  describe('Sessions', function() {
 
-  it('allow to authenticate with correct credentials', function(done) {
-    client.post('/sessions/', {username: 'admin', password: 'admin'}, function(err, req, res) {
-      assert.ifError(err);
-      assert.equal(res.statusCode, 201);
-      client.headers.cookie = res.headers['set-cookie'];
-      done();
+    it('allow to authenticate with wrong credentials', function(done) {
+      client.post('/sessions/', {username: 'pinochio', password: 'foobar'}, function(err, req, res, json) {
+        assert.equal(json.token, undefined);
+        assert.equal(res.statusCode, 403);
+        done();
+      });
     });
-  });
 
-  it('should return the associated user for a session', function(done) {
-    client.get('/sessions/current', function(err, req, res, json) {
-      assert.ifError(err);
-      assert.equal(res.statusCode, 200);
-      assert.equal(json.username, 'admin');
-      done();
+    it('allow to authenticate with correct credentials', function(done) {
+      client.post('/sessions/', {username: 'admin', password: 'admin'}, function(err, req, res) {
+        assert.ifError(err);
+        assert.equal(res.statusCode, 201);
+        client.headers.cookie = res.headers['set-cookie'];
+        done();
+      });
     });
-  });
 
-  it('should log out', function(done) {
-    client.del('/sessions/current', function(err, req, res) {
-      assert.equal(res.statusCode, 204);
-      done();
+    it('should return the associated user for a session', function(done) {
+      client.get('/sessions/current', function(err, req, res, json) {
+        assert.ifError(err);
+        assert.equal(res.statusCode, 200);
+        assert.equal(json.username, 'admin');
+        done();
+      });
     });
+
+    it('should log out', function(done) {
+      client.del('/sessions/current', function(err, req, res) {
+        assert.equal(res.statusCode, 204);
+        done();
+      });
+    });
+
   });
 
 });
--- test/resources/2_tags.js
+++ test/resources/2_tags.js
@@ -2,93 +2,97 @@ var assert = require('assert');
 var client = require('../client');
 var models = require('../../lib/models');
 
-describe('Tags', function() {
+describe('Resource', function() {
 
-  it('should not be possible to create tags when not logged it', function(done) {
-    client.post('/tags',
-      {title: 'Some other title'}, function(err, req, res) {
-        assert.equal(res.statusCode, 403);
-        done();
-      });
-  });
+  describe('Tags', function() {
 
-  it('should log in', function(done) {
-    client.post('/sessions/', {username: 'admin', password: 'admin'}, function(err, req, res) {
-      assert.equal(res.statusCode, 201);
-      client.headers.cookie = res.headers['set-cookie'];
-      done();
+    it('should not be possible to create tags when not logged it', function(done) {
+      client.post('/tags',
+        {title: 'Some other title'}, function(err, req, res) {
+          assert.equal(res.statusCode, 403);
+          done();
+        });
     });
-  });
 
-  it('should be possible to create tags when logged it', function(done) {
-    client.post('/tags',
-      {title: 'Some other title'}, function(err, req, res, json) {
-        assert.ifError(err);
+    it('should log in', function(done) {
+      client.post('/sessions/', {username: 'admin', password: 'admin'}, function(err, req, res) {
         assert.equal(res.statusCode, 201);
-        assert.notEqual(json.id, undefined);
+        client.headers.cookie = res.headers['set-cookie'];
         done();
       });
-  });
+    });
 
-  it('ignore creating a tag with an existing slug', function(done) {
-    client.post('/tags',
-      {title: 'tag 5'}, function(err, req, res) {
+    it('should be possible to create tags when logged it', function(done) {
+      client.post('/tags',
+        {title: 'Some other title'}, function(err, req, res, json) {
+          assert.ifError(err);
+          assert.equal(res.statusCode, 201);
+          assert.notEqual(json.id, undefined);
+          done();
+        });
+    });
+
+    it('ignore creating a tag with an existing slug', function(done) {
+      client.post('/tags',
+        {title: 'tag 5'}, function(err, req, res) {
+          assert.equal(res.statusCode, 200);
+          done();
+        });
+    });
+
+    it('should be possible to edit tags', function(done) {
+      client.put('/tags/11', {title: 'Some edited title 1'}, function(err, req, res) {
+        assert.equal(res.statusCode, 201);
+        done();
+      });
+    });
+
+    it('should be possible to view a single tag', function(done) {
+      client.get('/tags/some-other-title', function(err, req, res, json) {
         assert.equal(res.statusCode, 200);
+        assert.equal(json.title, 'Some edited title 1');
+        assert.equal(json.slug, 'some-other-title');
         done();
       });
-  });
-
-  it('should be possible to edit tags', function(done) {
-    client.put('/tags/11', {title: 'Some edited title 1'}, function(err, req, res) {
-      assert.equal(res.statusCode, 201);
-      done();
     });
-  });
 
-  it('should be possible to view a single tag', function(done) {
-    client.get('/tags/some-other-title', function(err, req, res, json) {
-      assert.equal(res.statusCode, 200);
-      assert.equal(json.title, 'Some edited title 1');
-      assert.equal(json.slug, 'some-other-title');
-      done();
+    it('should not be possible to view a non-existing tag', function(done) {
+      client.get('/tags/i-dont-exist', function(err, req, res, json) {
+        assert.equal(res.statusCode, 404);
+        assert.equal(json.message, 'Not found');
+        done();
+      });
     });
-  });
 
-  it('should not be possible to view a non-existing tag', function(done) {
-    client.get('/tags/i-dont-exist', function(err, req, res, json) {
-      assert.equal(res.statusCode, 404);
-      assert.equal(json.message, 'Not found');
-      done();
+    it('should be possible to view multiple tags at once', function(done) {
+      client.get('/tags', function(err, req, res, json) {
+        assert.equal(json.length, 11);
+        done();
+      });
     });
-  });
 
-  it('should be possible to view multiple tags at once', function(done) {
-    client.get('/tags', function(err, req, res, json) {
-      assert.equal(json.length, 11);
-      done();
+    it('should not be possible to remove a non-existing tag', function(done) {
+      client.del('/tags/50', function(err, req, res, json) {
+        assert.equal(res.statusCode, 404);
+        assert.equal(json.message, 'Not found');
+        done();
+      });
     });
-  });
 
-  it('should not be possible to remove a non-existing tag', function(done) {
-    client.del('/tags/50', function(err, req, res, json) {
-      assert.equal(res.statusCode, 404);
-      assert.equal(json.message, 'Not found');
-      done();
+    it('should be possible to remove a tag', function(done) {
+      client.del('/tags/3', function(err, req, res) {
+        assert.equal(res.statusCode, 204);
+        done();
+      });
     });
-  });
 
-  it('should be possible to remove a tag', function(done) {
-    client.del('/tags/3', function(err, req, res) {
-      assert.equal(res.statusCode, 204);
-      done();
+    it('should log out', function(done) {
+      client.del('/sessions/current', function(err, req, res) {
+        assert.equal(res.statusCode, 204);
+        done();
+      });
     });
-  });
 
-  it('should log out', function(done) {
-    client.del('/sessions/current', function(err, req, res) {
-      assert.equal(res.statusCode, 204);
-      done();
-    });
   });
 
 });
