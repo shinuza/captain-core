d8860faae0eb3b4d03d59027e30bf1038faca804
Samori Gorse <samorigorse@gmail.com>
2013-02-05 19:01:45 +0100

    Add posts -> tag association

--- test/resources/5_associations.js
+++ test/resources/5_associations.js
@@ -8,20 +8,20 @@ describe('Resource:', function() {
 
       describe('posts -> tags:', function() {
 
-        it('should get the associated posts to tag "Music" from its slug', function(done) {
-          client.get('/tags/music/posts/', function(err, req, res, json) {
+        it('should get the associated posts to tag "General" from its slug', function(done) {
+          client.get('/tags/general/posts/', function(err, req, res, json) {
             assert.ifError(err);
             assert.equal(res.statusCode, 200);
-            assert.equal(json.length, 2);
+            assert.equal(json.length, 3);
             done();
           });
         });
 
-        it('should get the associated posts to tag "Music" from its id', function(done) {
-          client.get('/tags/3/posts/', function(err, req, res, json) {
+        it('should get the associated posts to tag "General" from its id', function(done) {
+          client.get('/tags/2/posts/', function(err, req, res, json) {
             assert.ifError(err);
             assert.equal(res.statusCode, 200);
-            assert.equal(json.length, 2);
+            assert.equal(json.length, 3);
             done();
           });
         });
@@ -42,12 +42,26 @@ describe('Resource:', function() {
           });
         });
 
-        it('should get the associated tags to post "A blog post about HipHop" from its id', function(done) {
-          client.get('/posts/a-blog-post-about-hiphop/tags/', function(err, req, res, json) {
-            assert.ifError(err);
-            assert.equal(res.statusCode, 200);
-            assert.equal(json.length, 2);
+        it('should log in', function(done) {
+          client.post('/sessions/', {username: 'admin', password: 'admin'}, function(err, req, res) {
+            assert.equal(res.statusCode, 201);
+            client.headers.cookie = res.headers['set-cookie'];
             done();
+          });
+        });
+
+        it('should associate posts "A blog post about not publishing something" to tags "Programing" And "Music"', function(done) {
+          client.post('/posts/6/tags/', {data: [{id: 1}, {id: 3}]}, function(err, req, res, json) {
+            assert.ifError(err);
+            assert.equal(res.statusCode, 201);
+            assert.equal(json.count, 2);
+
+            client.get('/posts/6/tags/', function(err, req, res, json) {
+              assert.ifError(err);
+              assert.equal(res.statusCode, 200);
+              assert.equal(json.length, 2);
+              done();
+            });
           });
         });
 
