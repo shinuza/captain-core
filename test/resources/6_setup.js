7e37387792ce6bd5606cc8b43ed3cfb603c253e1
Samori Gorse <samorigorse@gmail.com>
2013-03-10 15:58:54 +0100

    Added test for EventSource format

--- test/resources/6_setup.js
+++ test/resources/6_setup.js
@@ -18,30 +18,31 @@ describe('Resource:', function() {
       });
     });
 
-    it('should create the tables', function(done) {
-      var client = restify.createStringClient({
-        url: 'http://localhost:' + settings.get('PORT')
-      });
-      client.get('/setup/table-creation', function(err, req, res) {
+    it('should generate the settings-file', function(done) {
+      client.post('/setup/commit', {uri: 'tcp://shinuza@localhost/shinuza'}, function(err, req, res) {
         assert.ifError(err);
-        assert.equal(res.headers['content-type'], "text/event-stream; charset=utf-8");
-        done();
-      });
-    });
-
-    it.skip('should generate the settings-file', function(done) {
-      client.get('/setup/write', function(err, req, res, json) {
-        assert.ifError(err);
-        assert.equal(res.statusCode, 200);
+        assert.equal(res.statusCode, 201);
         assert.equal(fs.existsSync(settings.path(settings.DEFAULT_FILENAME)), false);
         done();
       });
     });
 
-    it.skip('should create a user', function(done) {
-      client.post('/setup/create-user', {username: 'foo', password: 'bar'}, function(err, req, res, json) {
+    it('should create the tables', function(done) {
+      var client = restify.createStringClient({
+        url: 'http://localhost:' + settings.get('PORT')
+      });
+      client.post('/setup/table-creation', {uri: 'tcp://shinuza@localhost/shinuza'}, function(err, req, res) {
         assert.ifError(err);
-        assert.equal(res.statusCode, 200);
+        assert.equal(res.headers['content-type'], "text/event-stream; charset=utf-8");
+        client.close();
+        done();
+      });
+    });
+
+    it.skip('should create a user', function(done) {
+      client.post('/setup/create-user', {username: 'foo', password: 'bar'}, function(err, req, res) {
+        assert.ifError(err);
+        assert.equal(res.statusCode, 201);
         done();
       });
     });
