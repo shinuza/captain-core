e71154b7aa8588c5d873ff7a88b8a2472b355f17
Samori Gorse <samorigorse@gmail.com>
2013-03-23 01:18:55 +0100

    Re-organized utils

--- test/util.js
+++ test/util.js
@@ -1,19 +1,20 @@
 var assert = require('assert');
-var util = require('../lib/util');
+var date = require('../lib/util/date');
+var string = require('../lib/util/string');
 
 
 describe('Util', function() {
 
   it('should slugify the given string', function() {
-    var result = util.slugify("Un éléphant à l'orée du bois");
+    var result = string.slugify("Un éléphant à l'orée du bois");
     assert.equal(result, 'un-elephant-a-loree-du-bois');
   });
 
   it('should convert a timezone to its minutes equivalent', function() {
-    var result = util.tzToMinutes(),
-        result2 = util.tzToMinutes('America/Guyana'),
-        result3 = util.tzToMinutes('Asia/Tokyo'),
-        result4 = util.tzToMinutes('Lol/Cat');
+    var result = date.tzToMinutes(),
+        result2 = date.tzToMinutes('America/Guyana'),
+        result3 = date.tzToMinutes('Asia/Tokyo'),
+        result4 = date.tzToMinutes('Lol/Cat');
 
     assert.strictEqual(result, 0);
     assert.strictEqual(result2, 240);
@@ -30,7 +31,7 @@ describe('Util', function() {
     date2.setHours(date2.getHours() + 4);
     date2.setSeconds(date2.getSeconds() + 50);
 
-    result = util.stampify('2 days, 4 hours, 50 seconds', date);
+    result = date.stampify('2 days, 4 hours, 50 seconds', date);
 
     assert.equal(result.toJSON(), date2.toJSON());
   });
